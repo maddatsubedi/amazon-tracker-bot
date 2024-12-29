5a600751d25b5f51950b916a147e89dfbb8ca4d6
Maddat Subedi <163100085+maddatsubedi@users.noreply.github.com>
2024-12-29 23:38:25 +0545

    asdf

--- tracking/notify.js
+++ tracking/notify.js
@@ -1,4 +1,20 @@
+const { getDealEmbed } = require("../embeds/dealsEmbeds");
+
 const notify = async (deal) => {
+
+    const range = await getRangeForDiscount(deal[maxPriceAccesors[0]].percentageDropDay);
+
+    if (!range) {
+        return {
+            error: true,
+            errorType: 'NO_RANGE_CONFIGURED'
+        }
+    }
+
+    const roleID = range.roleID;
+    const channelID = range.channelID;
+
+    const dealEmbed = getDealEmbed(deal, roleID);
 
 }
 
